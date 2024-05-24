872fd0895d2a85bc3ba36d33cdca2b6d2392503d
ALEXANDER FIGUEREDO <alexanderfigueredo.gms@gmail.com>
2024-05-24 11:24:08 -0500

    maquedado de FQA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,7 @@ export class AppComponent implements OnInit {
   constructor(private _router: Router) {}
 
   ngOnInit(): void {
-    this._router.navigateByUrl('/home');
-    // this._router.navigateByUrl('/about-us');
+    // this._router.navigateByUrl('/home');
+    this._router.navigateByUrl('/fqa');
   }
 }
