ca1ff08319129422ff94b43bf379ce5a0908d4c8
ALEXANDER FIGUEREDO <alexanderfigueredo.gms@gmail.com>
2024-06-06 19:36:36 -0500

    Refactor servise  component and tsconfig.json, se agrego carrusel con jquery y ngx-slink-carousel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { FaqComponent } from './components/faq/faq.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { BlogComponent } from './components/blog/blog.component';
 
 export const routes: Routes = [
@@ -26,11 +25,6 @@ export const routes: Routes = [
     path: 'fqa',
     title: 'FQA',
     component: FaqComponent,
-  },
-  {
-    path: 'contact',
-    title: 'Contact',
-    component: ContactComponent,
   },
   {
     path: 'blog',
--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,12 +1,122 @@
 import { Component } from '@angular/core';
 
+// Import Slick carousel module
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
+/**
+ * Component for displaying services.
+ */
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [SlickCarouselModule],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrl: './services.component.css',
 })
 export class ServicesComponent {
+  // First carousel -------------------------------
 
+  /**
+   * Array of slides for the first carousel.
+   */
+  public slides = [
+    { img: '../../../assets/images/Logos/Lunika.png' },
+    { img: '../../../assets/images/Logos/OdontoMigue.png' },
+    { img: '../../../assets/images/Logos/Alactus.png' },
+    { img: '../../../assets/images/Logos/Agathos .png' },
+    { img: '../../../assets/images/Logos/ms-consulting.jpg' },
+    { img: '../../../assets/images/Logos/frank-Adam.png' },
+  ];
+
+  // Slick carousel configuration
+
+  /**
+   * Configuration options for the first carousel.
+   */
+  public slideConfig = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    slickPlay: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  // Second carousel -------------------------------
+
+  /**
+   * Array of slides for the second carousel.
+   */
+  public slides2 = [
+    { img: '../../../assets/images/Logos/logo3.png' },
+    { img: '../../../assets/images/Logos/HIAS.png' },
+    { img: '../../../assets/images/Logos/denaro.png' },
+    { img: '../../../assets/images/Logos/tanira.png' },
+    { img: '../../../assets/images/Logos/frank-Adam.png' },
+    { img: '../../../assets/images/Logos/logo aszero .png' },
+  ];
+
+  // Slick carousel configuration
+
+  /**
+   * Configuration options for the second carousel.
+   */
+  public slideConfig2 = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    slickPlay: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 }
